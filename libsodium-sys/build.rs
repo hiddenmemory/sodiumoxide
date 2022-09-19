4f2ce387043c3f669e33700cbe43dc51de4c91dc
Chris Ross <chris@hiddenmemory.co.uk>
2022-09-19 14:19:23 +0100

    Simulator check again

--- libsodium-sys/build.rs
+++ libsodium-sys/build.rs
@@ -189,7 +189,7 @@ fn make_libsodium(target: &str, source_dir: &Path, install_dir: &Path) -> PathBu
             }
             "aarch64-apple-ios-sim" => {
                 cflags += " -arch arm64";
-                cflags += &format!(" -isysroot {}", sdk_dir_ios);
+                cflags += &format!(" -isysroot {}", sdk_dir_simulator);
                 cflags += &format!(" -mios-simulator-version-min={}", ios_simulator_version_min);
                 host_arg = "--host=arm-apple-darwin10".to_string();
             }
